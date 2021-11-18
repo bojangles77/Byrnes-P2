6002fc15399f9c9826e0c0c1fb6b3649a0c7bc80
Michael Byrnes <122mbyrnes@frhsd.com>
2021-11-18 09:16:43 -0500

    2.3.13-2.5.1

--- js/gallery.js
+++ js/gallery.js
@@ -46,15 +46,24 @@ function swapPhoto() {
 	//with a new image from your images array which is loaded
 	//from the JSON string
 	console.log('swap photo');
-  var mLastFrameTime = 0
-  mCurrentIndex + 1
+  var mLastFrameTime = 0;
+  mCurrentIndex += 1;
 }
+
+
+
 
 // Counter for the mImages array
 var mCurrentIndex = 0;
 
+
+
+
 // XMLHttpRequest variable
 var mRequest = new XMLHttpRequest();
+
+
+
 
 mRequest.addEventListener("readystatechange", () => {
   if (mRequest.readyState === 4 && request.status === 200) {
@@ -69,18 +78,30 @@ mRequest.open("GET", "images.JSON");
 mRequest.send();
 
 
+
+
+
 // Array holding GalleryImage objects (see below).
 var mImages = ["location" "description" "date"];
   mImages[0] = document.getElementsByClassName("location").innerHTML = "location: " + mImages[mCurrentIndex].location;
   mImages[1] = document.getElementsByClassName("description").innerHTML = "description: " + mImages[mCurrentIndex].description;
   mImages[2] = document.getElementsByClassName("date").innerHTML = "date: " + mImages[mCurrentIndex].date;
 
+
+
+
 // Holds the retrived JSON information
 var mJson;
+
+
+
 
 // URL for the JSON to load by default
 // Some options for you are: images.json, images.short.json; you will need to create your own extra.json later
 var mUrl = 'insert_url_here_to_image_json';
+
+
+
 
 
 //You can optionally use the following function as your event callback for loading the source of Images from your json data (for HTMLImageObject).
@@ -92,18 +113,35 @@ function makeGalleryImageOnloadCallback(galleryImage) {
 	}
 }
 
-$(document).ready( function() {
 
+
+
+$(document).ready( function() {
+  function fetchJSON("mJson");
 	// This initially hides the photos' metadata information
 	$('.details').eq(0).hide();
 
 });
+
+
+
 
 window.addEventListener('load', function() {
 
 	console.log('window loaded');
 
 }, false);
+
+
+
+
+function iterateJSON() {
+  for (let x = 0; x < mJson.length; x++) {
+  ;
+}
+}
+
+
 
 function GalleryImage() {
     this.location
